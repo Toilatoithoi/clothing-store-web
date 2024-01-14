2ac806dff9e931feb584263a80cb761caa49f38a
thongnv <thong.nv173390@gmail.com>
2024-01-14 23:19:43 +0700

    add api update user

---
+++ src/app/api/user/[userId]/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/db';
+
+// Update user info
+export const PUT = async (req: NextRequest, { params }: { params: { userId: string; } }) => {
+  console.log({ params })
+  const id = Number(params.userId);
+  const body = await req.json();
+  // validate data: phoneNumber có hợp lệ k? .....
+
+  try {
+    const user = await prisma.user.findFirst({ where: { id } });
+    // lây thông tin user trên db -> nếu mà không có thông tin user -> thông báo lỗi user not exist
+    if (user == null) {
+      return NextResponse.json({
+        code: "USER_NOT_EXIST",
+        message: "User không tồn tại"
+      }, { status: 403 })
+    }
+    // update data vào hệ thống
+    const res = await prisma.user.update({
+      where: {
+        id
+      },
+      data: {
+        name: body.name,
+        dob: body.dob,
+        phoneNumber: body.phoneNumber,
+      }
+    })
+
+    return NextResponse.json({ id: res.id })
+  } catch (error) {
+    return NextResponse.json({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Lỗi hệ thống"
+    }, { status: 500 })
+  }
+}
+
+
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -9,4 +9,3 @@ export const GET = async () => {
 
 
 
-
