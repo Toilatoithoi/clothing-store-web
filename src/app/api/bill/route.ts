42cdb0939d95081dcc540fcfa1c5290fd9a6414d
thongnv <thong.nv173390@gmail.com>
2024-01-17 21:50:22 +0700

    update api bill

--- src/app/api/bill/route.ts
+++ src/app/api/bill/route.ts
@@ -2,65 +2,78 @@ import prisma from '@/lib/db';
 import { isBlank } from '@/utils';
 import { NextRequest, NextResponse } from 'next/server';
 import { CreateBillReq } from '@/interfaces/request';
+import { verifyToken } from '@/utils/service';
 export const GET = async (req: NextRequest) => {
   try {
-    const bill= await prisma.bill.findMany();
+
+    const data = await verifyToken(req);
+    if (data == null) {
+      return NextResponse.json({ code: 'UNAUTHORIZED' }, { status: 400 })
+    }
+
+    const bill = await prisma.bill.findMany({
+      where: {
+        user: {
+          username: data.username
+        }
+      }
+    });
 
     return NextResponse.json(bill);
   } catch (error) {
-    console.log({error})
+    console.log({ error })
     return NextResponse.json({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Lỗi hệ thống"
-      }, { status: 500 })
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Lỗi hệ thống"
+    }, { status: 500 })
   }
 
 }
 
 export const POST = async (req: NextRequest) => {
-    //validate input 
-    const body : CreateBillReq = await req.json();
-    if (isBlank(String(body.user_id)) || isBlank(body.city) || isBlank(body.district) || isBlank(body.wards) || isBlank(body.address)) {
-      return NextResponse.json({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: 'Đầu vào không hợp lệ'
-      }, {
-        status: 400
-      })
-    }
-
-  
-    try {
-        // check bill có thành công không 
-  
-        const billTue = await prisma.bill.findFirst({
-           where: {
-            status: "sccess"
-           }
-        })
-    
-        if (billTue == null) {
-           return NextResponse.json({
-            code: 'Bill is false',
-        }, {
-            status: 404
-        })
-      }
-
-      // thêm vào db
-    const createdBill = await prisma.bill.create({
-        data: {
-          ...body,
-        }
-      })
-    
-      return NextResponse.json({createdBill})
-
-    } catch (error) {
-        console.log({error})
-        return NextResponse.json({
-            code: "INTERNAL_SERVER_ERROR",
-            message: "Lỗi hệ thống"
-          }, { status: 500 })  
-    }
+  //validate input 
+  const body: CreateBillReq = await req.json();
+  if (isBlank(String(body.user_id)) || isBlank(body.city) || isBlank(body.district) || isBlank(body.wards) || isBlank(body.address)) {
+    return NextResponse.json({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'Đầu vào không hợp lệ'
+    }, {
+      status: 400
+    })
   }
+
+
+  try {
+    // check bill có thành công không 
+
+    const billTue = await prisma.bill.findFirst({
+      where: {
+        status: "sccess"
+      }
+    })
+
+    if (billTue == null) {
+      return NextResponse.json({
+        code: 'Bill is false',
+      }, {
+        status: 404
+      })
+    }
+
+    // thêm vào db
+    const createdBill = await prisma.bill.create({
+      data: {
+        ...body,
+      }
+    })
+
+    return NextResponse.json({ createdBill })
+
+  } catch (error) {
+    console.log({ error })
+    return NextResponse.json({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Lỗi hệ thống"
+    }, { status: 500 })
+  }
+}
--- src/utils/service.ts
+++ src/utils/service.ts
@@ -1,5 +1,27 @@
+import { User } from '@prisma/client';
 import sha256 from 'crypto-js/sha256';
+import { NextRequest } from 'next/server';
+import jwt from 'jsonwebtoken'
 
 export const hashPassword = (password: string) => {
   return sha256(password).toString();
+}
+
+
+export const verifyToken = async (req: NextRequest) => {
+  const authorization = req.headers.get('authorization');
+  if (!authorization) {
+    return null
+  }
+  const [tokenType, accessToken] = authorization.split(' ');
+
+  if (tokenType !== 'Bearer' || !accessToken) {
+    return null
+  }
+  try {
+    const data = await jwt.verify(accessToken, process.env.JWT_SECRET_KEY ?? '') as User;
+    return data
+  } catch (error) {
+    return null
+  }
 }
