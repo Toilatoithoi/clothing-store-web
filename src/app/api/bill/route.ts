b1f88cc2d298effb91d5f4256c73816ee4efe02a
duchoangkim123 <duc42165@huce.edu.vn>
2024-01-17 22:20:09 +0700

    sửa status

--- src/app/api/bill/route.ts
+++ src/app/api/bill/route.ts
@@ -35,7 +35,7 @@ export const POST = async (req: NextRequest) => {
   
         const billTue = await prisma.bill.findFirst({
            where: {
-            status: "sccess"
+            status: "SCCESS"
            }
         })
     
