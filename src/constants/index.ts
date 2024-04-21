5cb2e15c4f53f3e6b8331b4a5ae6730698debb40
duchoangkim123 <duc42165@huce.edu.vn>
2024-04-21 22:51:52 +0700

    sửa lỗi

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 // constant nơi chứa các biến hằng số
 // số bản ghi lấy ra trong 1 lần request
 // export để có thể dùng nhiều chỗ
-export const FETCH_COUNT = 8;
+export const FETCH_COUNT = 50;
 
 // nếu bên chỗ viết hàm chỉ export thì sẽ gọi thể kiểu import {} from 
 // import {formatNumber} from '@/utils/index';
